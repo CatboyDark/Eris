3507d98b6dc766e0451cbd8468799b473a5b47e5
CatboyDark <dqrkofficial@protonmail.com>
2024-07-19 03:32:39 -0700

    reformatted help.js. no more redundancy

--- src/discord/cmds/slash/help.js
+++ src/discord/cmds/slash/help.js
@@ -1,55 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { createMsg, createRow } = require('../../../builder.js');
-const { readConfig } = require('../../../configUtils.js');
-const fs = require('fs');
-const path = require('path');
-
-function loadCmds()
-{
-	const commandFilesDir = __dirname;
-
-	return fs.readdirSync(commandFilesDir)
-		.map(file => require(path.join(commandFilesDir, file)))
-		.filter(command => command && command.type && command.data);
-}
-
-const buttons = createRow([
-	{id: 'mccmds', label: 'Ingame Commands', style: 'Primary'},
-	{id: 'credits', label: 'Credits', style: 'Success'},
-	{id: 'support', label: 'Support', style: 'Success'}
-]);
-
-const formatCommands = (cmds, staffOnly = false) =>
-	cmds.filter(cmd => staffOnly ? cmd.staff : !cmd.staff)
-		.sort((a, b) => a.data.name.localeCompare(b.data.name))
-		.map(cmd => `* **\`/${cmd.data.name}\`** ${cmd.data.description}`);
-
-async function help(interaction)
-{
-	const config = readConfig();
-	const cmds = loadCmds();
-	const roles = interaction.member.roles.cache;
-	const isStaff = roles.some(role => config.staffRole.includes(role.id));
-		  
-	const nonCommands = `**Commands**\n${formatCommands(cmds)}`;
-	const staffCommands = isStaff ? `\n\n**Staff**\n${formatCommands(cmds, true)}` : '';
-		  
-	const embed = createMsg({
-			  color: config.colorTheme,
-			  icon: config.guildIcon,
-			  title: config.guild,
-			  description: `${nonCommands}${staffCommands}`,
-			  footer: 'Created by @CatboyDark',
-			  footerIcon: 'https://i.imgur.com/4lpd01s.png'
-	});
-		  
-	await interaction.update({ embeds: [embed], components: [buttons] });
-}
+const { createHelpMsg, helpButtons } = require('../../logic/help.js');
 
 module.exports = 
 {
-	help,
-
 	type: 'slash',
 	staff: false,
 	data: new SlashCommandBuilder()
@@ -58,6 +11,7 @@ module.exports =
 		
 	async execute(interaction) 
 	{
-		help(interaction);
+		const embed = await createHelpMsg(interaction);
+		await interaction.reply({ embeds: [embed], components: [helpButtons] });
 	}
 };
--- src/discord/logic/help.js
+++ src/discord/logic/help.js
@@ -1,10 +1,48 @@
-const { createMsg, createRow } = require('../../builder');
-const { readConfig } = require('../../configUtils');
-const { updateHelp } = require('../cmds/slash/help.js');
+const { createMsg, createRow } = require('../../builder.js');
+const { readConfig } = require('../../configUtils.js');
+const fs = require('fs');
+const path = require('path');
+
+async function createHelpMsg(interaction)
+{
+	const config = readConfig();
+
+	const cmdsDirectory = path.join(__dirname, '..', 'cmds', 'slash');
+	const cmds = fs.readdirSync(cmdsDirectory)
+		.map(file => require(path.join(cmdsDirectory, file)))
+		.filter(command => command && command.type && command.data);
+
+	const isStaff = interaction.member.roles.cache.some(role => config.staffRole.includes(role.id));
+
+	const formatCommands = (staffOnly = false) =>
+		cmds
+			.filter(cmd => staffOnly ? cmd.staff : !cmd.staff)
+			.sort((a, b) => a.data.name.localeCompare(b.data.name))
+			.map(cmd => `* **\`/${cmd.data.name}\`** ${cmd.data.description}`);
+
+	const nonCommands = `**Commands**\n${formatCommands()}`;
+	const staffCommands = isStaff ? `\n\n**Staff**\n${formatCommands(true)}` : '';
+
+	return createMsg({
+		color: config.colorTheme,
+		icon: config.guildIcon,
+		title: config.guild,
+		description: `${nonCommands}${staffCommands}`,
+		footer: 'Created by @CatboyDark',
+		footerIcon: 'https://i.imgur.com/4lpd01s.png'
+	});
+}
+
+const helpButtons = createRow([
+	{id: 'mccmds', label: 'Ingame Commands', style: 'Primary'},
+	{id: 'credits', label: 'Credits', style: 'Success'},
+	{id: 'support', label: 'Support', style: 'Success'}
+]);
 
 async function cmds(interaction) 
 {
-	await updateHelp(interaction);
+	const embed = await createHelpMsg(interaction);
+	await interaction.update({ embeds: [embed], components: [helpButtons] });
 }
 
 async function mccmds(interaction) 
@@ -59,4 +97,4 @@ async function support(interaction)
 	interaction.update({ embeds: [supportMsg], components: [buttons] });
 }
 
-module.exports = { cmds, mccmds, credits, support };
+module.exports = { createHelpMsg, helpButtons, cmds, mccmds, credits, support };
