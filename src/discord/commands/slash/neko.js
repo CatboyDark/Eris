b23e968037594efd482d3010305d7cb809139e97
CatboyDark <161413864+CatboyDark@users.noreply.github.com>
2025-12-21 17:14:04 -0800

    Fix image fetching in neko command

--- src/discord/commands/slash/neko.js
+++ src/discord/commands/slash/neko.js
@@ -14,8 +14,9 @@ export default {
 	async execute(interaction) {
 		const nsfw = interaction.options.getString('nsfw') ?? false;
 
-		const id = await fetch(`https://nekos.moe/api/v1/random/image?nsfw=${nsfw}&count=1`)
+		const response = await fetch(`https://nekos.moe/api/v1/random/image?nsfw=${nsfw}&count=1`)
+		const data = response.json()
 
-		await interaction.reply(createMsg([[{ img: `https://nekos.moe/image/${id}` }]]))
+		await interaction.reply(createMsg([[{ img: `https://nekos.moe/image/${data.images[0].id}` }]]))
 	}
 };
