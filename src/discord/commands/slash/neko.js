e2ebe1b2222c142b0af2fff4079675f16d79b4ef
CatboyDark <161413864+CatboyDark@users.noreply.github.com>
2025-12-21 19:39:02 -0800

    Fix reply message format in neko command

--- src/discord/commands/slash/neko.js
+++ src/discord/commands/slash/neko.js
@@ -14,7 +14,7 @@ export default {
 	async execute(interaction) {
 		const nsfw = interaction.options.getString('nsfw');
 		if (nsfw) {
-			return await interaction.reply(createMsg([[{ img: 'https://tenor.com/view/vorzek-vorzneck-oglg-og-lol-gang-gif-24901093' }]]))
+			return await interaction.reply('https://tenor.com/view/vorzek-vorzneck-oglg-og-lol-gang-gif-24901093')
 		}
 
 		const response = await fetch(`https://nekos.moe/api/v1/random/image?nsfw=false&count=1`)
