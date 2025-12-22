ef2f06e85ec14118dfcffbfd50a571c61823b839
CatboyDark <161413864+CatboyDark@users.noreply.github.com>
2025-12-21 19:31:36 -0800

    Enable NSFW option in neko command

--- src/discord/commands/slash/neko.js
+++ src/discord/commands/slash/neko.js
@@ -3,16 +3,19 @@ import { createMsg } from '../../../utils/utils.js';
 export default {
 	name: 'neko',
 	desc: 'nyaaa~',
-	// options: [
-	// 	{ type: 'string', name: 'nsfw', desc: 'NSFW', required: false,
-	// 		choices: [
-	// 			{ name: 'true', value: 'true' }
-	// 		]
-	// 	}
-	// ],
+	options: [
+		{ type: 'string', name: 'nsfw', desc: 'NSFW', required: false,
+			choices: [
+				{ name: 'true', value: 'true' }
+			]
+		}
+	],
 
 	async execute(interaction) {
-		const nsfw = interaction.options.getString('nsfw') ?? false;
+		const nsfw = interaction.options.getString('nsfw');
+		if (nsfw) {
+			return await interaction.reply(createMsg([[{ img: 'https://tenor.com/view/vorzek-vorzneck-oglg-og-lol-gang-gif-24901093' }]]))
+		}
 
 		const response = await fetch(`https://nekos.moe/api/v1/random/image?nsfw=false&count=1`)
 		const data = await response.json()
