f316949529ef5cfd49deebb592c1fbcce3bf979d
CatboyDark <161413864+CatboyDark@users.noreply.github.com>
2025-12-21 17:08:09 -0800

    Add Neko command

---
+++ src/discord/commands/slash/neko.js
@@ -0,0 +1,21 @@
+import { createMsg } from '../../../utils/utils.js';
+
+export default {
+	name: 'neko',
+	desc: 'nyaaa~',
+	options: [
+		{ type: 'string', name: 'nsfw', desc: 'NSFW', required: false,
+			choices: [
+				{ name: 'true', value: 'true' }
+			]
+		}
+	],
+
+	async execute(interaction) {
+		const nsfw = interaction.options.getString('nsfw') ?? false;
+
+		const id = await fetch(`https://nekos.moe/api/v1/random/image?nsfw=${nsfw}&count=1`)
+
+		await interaction.reply(createMsg([[{ img: `https://nekos.moe/image/${id}` }]]))
+	}
+};
