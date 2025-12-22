24fafccbb097a45f1defd3e9b51fc4c250041d13
CatboyDark <161413864+CatboyDark@users.noreply.github.com>
2025-12-21 19:13:20 -0800

    Comment out NSFW option in neko command

--- src/discord/commands/slash/neko.js
+++ src/discord/commands/slash/neko.js
@@ -3,18 +3,18 @@ import { createMsg } from '../../../utils/utils.js';
 export default {
 	name: 'neko',
 	desc: 'nyaaa~',
-	options: [
-		{ type: 'string', name: 'nsfw', desc: 'NSFW', required: false,
-			choices: [
-				{ name: 'true', value: 'true' }
-			]
-		}
-	],
+	// options: [
+	// 	{ type: 'string', name: 'nsfw', desc: 'NSFW', required: false,
+	// 		choices: [
+	// 			{ name: 'true', value: 'true' }
+	// 		]
+	// 	}
+	// ],
 
 	async execute(interaction) {
 		const nsfw = interaction.options.getString('nsfw') ?? false;
 
-		const response = await fetch(`https://nekos.moe/api/v1/random/image?nsfw=${nsfw}&count=1`)
+		const response = await fetch(`https://nekos.moe/api/v1/random/image?nsfw=false&count=1`)
 		const data = await response.json()
 
 		await interaction.reply(createMsg([[{ img: `https://nekos.moe/image/${data.images[0].id}` }]]))
