8a0be383827e4e61d44ed44bd5a87fed6c330dac
CatboyDark <161413864+CatboyDark@users.noreply.github.com>
2025-12-21 17:16:50 -0800

    Fix async fetch response handling in neko command

--- src/discord/commands/slash/neko.js
+++ src/discord/commands/slash/neko.js
@@ -15,7 +15,7 @@ export default {
 		const nsfw = interaction.options.getString('nsfw') ?? false;
 
 		const response = await fetch(`https://nekos.moe/api/v1/random/image?nsfw=${nsfw}&count=1`)
-		const data = response.json()
+		const data = await response.json()
 
 		await interaction.reply(createMsg([[{ img: `https://nekos.moe/image/${data.images[0].id}` }]]))
 	}
