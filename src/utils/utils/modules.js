2225c036e884fccd5813aef473a2e2ffe5987c7a
CatboyDark <CatboyDark@users.noreply.github.com>
2026-02-10 02:13:40 -0800

    meow

--- src/utils/utils/modules.js
+++ src/utils/utils/modules.js
@@ -1,23 +1,23 @@
-// import { Config } from './../utils.js';
-// import { meow } from '../../modules/meow.js';
-// import { colon_three } from '../../modules/colon_three.js';
-// import { minecraft } from '../../minecraft/Minecraft.js';
+import { Config } from './../utils.js';
+import { meow } from '../../modules/meow.js';
+import { colon_three } from '../../modules/colon_three.js';
+import { minecraft } from '../../minecraft/Minecraft.js';
 
-// export { loadFunny, customCommands };
+export { loadFunny, customCommands };
 
-// const loadFunny = {
-// 	async discord(message) {
-// 		if (Config.funny.meow) await meow.discord(message);
-// 		if (Config.funny.colon_three) await colon_three.discord(message);
-// 	},
+const loadFunny = {
+	async discord(message) {
+		if (Config.funny.meow) await meow.discord(message);
+		if (Config.funny.colon_three) await colon_three.discord(message);
+	},
 
-// 	async minecraft(message) {
-// 		if (!message.channel || message.sender === minecraft.username || message.event) return;
+	async minecraft(message) {
+		if (!message.channel || message.sender === minecraft.username || message.event) return;
 
-// 		if (Config.funny.meow) await meow.minecraft(message);
-// 		if (Config.funny.colon_three) await colon_three.minecraft(message);
-// 	}
-// };
+		if (Config.funny.meow) await meow.minecraft(message);
+		if (Config.funny.colon_three) await colon_three.minecraft(message);
+	}
+};
 
 // const customCommands = {
 // 	async minecraft(message) {
