9703a21ab26c771f8ca8086f7a4979e0d815c7af
CatboyDark <CatboyDark@users.noreply.github.com>
2026-02-27 23:56:31 -0800

    meow

--- src/modules/bridge.js
+++ src/modules/bridge.js
@@ -140,7 +140,7 @@ async function DCbridge(m) {
 		let content;
 		if (m.reference) {
 			const originalMessage = await m.channel.messages.fetch(m.reference.messageId);
-			targetUser = originalMessage.author.bot ?
+			const targetUser = originalMessage.author.bot ?
 				originalMessage.embeds?.[0]?.data.author.name.split(' ')?.[0] ??
 				originalMessage.attachments.first()?.name?.replace('.png', '') ??
 				originalMessage.member?.displayName :
