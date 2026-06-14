b0428b4b52ed2b2a52ea11fb24b6cf8bae4ea775
CatboyDark <CatboyDark@users.noreply.github.com>
2026-06-14 11:27:08 -0700

    meow

--- start.js
+++ start.js
@@ -3,9 +3,19 @@ import { Minecraft } from './src/minecraft/Minecraft.js';
 import { Mongo } from './src/mongo/Mongo.js';
 
 export async function start() {
+	process.on('unhandledRejection', (reason, promise) => {
+		console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+	});
+
+	process.on('uncaughtException', (err) => {
+		console.error('There was an uncaught error', err);
+		process.exit(1);
+	});
+
 	await Mongo();
 	await Discord();
 	await Minecraft();
+
 }
 
 start();
